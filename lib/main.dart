72c62af4760e98a2030a5d615b88f3dd6fc35867
iloyadayvo <iloyoselumese@gmail.com>
2023-09-28 22:27:54 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:namer_app/test.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: const test(),
+     theme: ThemeData.light(useMaterial3: true)
+    );
+ }
+}
